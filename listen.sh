0e1d85830314bdb1fd7fdc245c5679724b9c5674
YD1RUH <32731955+YD1RUH@users.noreply.github.com>
2024-03-25 04:42:45 +0700

    Update listen.sh

--- listen.sh
+++ listen.sh
@@ -1,13 +1,13 @@
 while true
 do
 	#create folder
-	./amodem recv -v -o /tmp/info.rx
+	amodem recv -v -o /tmp/info.rx
 	mkdir $(pwd)/recv/$(cat /tmp/info.rx)
 	#header file
-	./amodem recv -v -o /tmp/data.rx
+	amodem recv -v -o /tmp/data.rx
 	echo "" > $(pwd)/recv/$(cat /tmp/info.rx)/$(cat /tmp/data.rx)
 	#content file
-	./amodem recv -v -o /tmp/data2.rx
+	amodem recv -v -o /tmp/data2.rx
 	cat /tmp/data2.rx > $(pwd)/recv/$(cat /tmp/info.rx)/$(cat /tmp/data.rx)
 	rm /tmp/*.rx
 	#clear
